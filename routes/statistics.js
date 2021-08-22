dc65b775dc3129be8f7d666a68fd7b310b582112
John Laird <lairdj@coventry.ac.uk>
2021-08-23 00:05:02 +0100

    Updated file to allow the request from API to display data AND store specific data into db that can be referenced later by user

--- routes/statistics.js
+++ routes/statistics.js
@@ -2,13 +2,16 @@
 const Router = require('koa-router');
 const bodyParser = require('koa-bodyparser');
 var axios = require("axios").default; // Needed for external API call
+const model = require('../models/stats');
 
 // Prefixed route
 const router = Router({prefix: '/myAPI/v1'});
 
 // Endpoints
-router.get('/stats', displayStats);
-router.get('/stats/COD', getCODStats);
+// router.get('/stats', displayStats);
+// router.get('/stats/COD', getCODStats);
+router.post('/stats', bodyParser(), getCODStats)
+router.post('/stats/myProfile/:id([0-9]{1,})', bodyParser(), getCODStats)
 
 
 function displayStats(ctx){
@@ -27,21 +30,35 @@ function displayStats(ctx){
 // https://rapidapi.com/elreco/api/call-of-duty-modern-warfare
 
 async function getCODStats(ctx){
+    // Get username and platform that will be needed for request
+    var username = ctx.request.body.username;
+    var platform = ctx.request.body.platform;
+    // Store HTTP Get to API with url and headers required to make call
     var options = {
         method: 'GET',
-        url: 'https://call-of-duty-modern-warfare.p.rapidapi.com/warzone/VanQ%20John/xbl',
+        url: `https://call-of-duty-modern-warfare.p.rapidapi.com/warzone/${username}/${platform}`,
         headers: {
             'x-rapidapi-host': 'call-of-duty-modern-warfare.p.rapidapi.com',
             'x-rapidapi-key': 'd08f4aa168msh70aa02d0584b683p1ac8bcjsnaa334409768f'
         }
     };
     
+    // Try to make request with all options specified above and store/display data to user
     try{
         await axios.request(options).then(function (response) {
-            console.log(response.data)
             ctx.status = 200
             ctx.body = response.data
+            // Key Values we want to store for user from request
+            if(ctx.params.id){
+                let values = {ID: ctx.params.id, wins: response.data.br.wins, kills: response.data.br.kills,
+                    kd: response.data.br.kdRatio, kpm: response.data.br.kills / response.data.br.gamesPlayed,
+                    userID: parseInt(ctx.params.id)}
+                saveData(values)
+            } else {
+                console.log("No Data")
+            }
         })
+    // If there was an error in the request display simple error message
     } catch(error){
         ctx.status = 404
         ctx.body = "Unable to perform action"
@@ -49,5 +66,12 @@ async function getCODStats(ctx){
     }
 }
 
-
+// Function stores data from API call into local DB for users profile
+async function saveData(data){
+    try{
+        await model.saveRecentStats(data)
+    } catch(error){
+        throw error
+    }
+}
 module.exports = router;
