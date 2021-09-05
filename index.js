048184d70caabffab5ea6c6b21e6e5a33ae99af3
John Laird <lairdj@coventry.ac.uk>
2021-09-05 21:59:37 +0100

    Updated

--- index.js
+++ index.js
@@ -8,12 +8,14 @@ const router = new Router();
 // Routes
 const stats = require('./routes/statistics');
 const dummy = require('./routes/dummyData');
+const user = require('./routes/user');
 
 
 port = 4000;
 app.use(router.routes());
 app.use(stats.routes());
 app.use(dummy.routes());
+app.use(user.routes())
 
 app.listen(port)
 console.log("Now listening on Port: ", port);
