5cc1cfad33cee093c861c8937e6e2025cb6528be
John Laird <lairdj@coventry.ac.uk>
2021-09-05 21:58:55 +0100

    added await as previously forgot

--- models/stats.js
+++ models/stats.js
@@ -3,6 +3,12 @@ const db = require('../helpers/database')
 // Store data into db or replace the original entry with newer data
 exports.saveRecentStats = async function saveRecentStats(values){
     let query = 'REPLACE INTO stats SET?;'
-    let data = db.execute_query(query, values);
+    let data = await db.execute_query(query, values);
+    return data;
+}
+
+exports.myStats = async function myStats(id){
+    let query = 'SELECT * FROM stats WHERE ID = ?;'
+    let data = await db.execute_query(query, id)
     return data;
 }
